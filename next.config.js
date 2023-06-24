6baae7574cb37cffa337c7d8404637b934bcd868
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-06-24 23:46:33 +0530

    ignoring warnings for build

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
       }
     ]
   },
-  reactStrictMode: true,
+  reactStrictMode: false,
 
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
