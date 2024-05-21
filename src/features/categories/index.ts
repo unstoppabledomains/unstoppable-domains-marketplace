e106bed50f095db060438a6d25b4e09638a5fdbd
Ben Behrman <bbehrman10@gmail.com>
2024-05-21 09:57:03 -0400

    Update index.ts

--- src/features/categories/index.ts
+++ src/features/categories/index.ts
@@ -222,7 +222,7 @@ var dappstoreMaping = `{
 
   "Gaming": ["games.strategy","games.simulation","games.role-playing","games.studios","games.action","games.racing","games.adventure","games.puzzle","games.board","games.word"],
 
-  "Naming Services": "naming-services",
+  "Naming Services (TLDs)": "naming-services",
   
   "NFT Marketplaces": "nft.nft-marketplaces",
 
