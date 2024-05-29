b2a8de596923514fd16615d3ba80fe285d68a3f6
Ben Behrman <bbehrman10@gmail.com>
2024-05-29 11:07:56 -0400

    Update custom_categories.ts

--- src/features/dapp/custom_categories.ts
+++ src/features/dapp/custom_categories.ts
@@ -34,7 +34,7 @@ export const categories = {
       subCategory: [],
     },
     {
-      category: "TLDs & Naming Services",
+      category: "Naming Services (TLDs)",
       subCategory: [],
     },
     {
