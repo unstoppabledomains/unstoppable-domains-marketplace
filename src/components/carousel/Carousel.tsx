6c0e9f87e3c8dfa7e3eb38b9958ba5928a52da76
Ben Behrman <bbehrman10@gmail.com>
2024-09-26 08:30:41 -0600

    Update Carousel.tsx

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -38,6 +38,13 @@ const Carousel: React.FC<PropType> = (props) => {
   } = usePrevNextButtons(emblaApi, onButtonClick)
 */
   const bannerData = [
+     {
+      "image": "https://storage.googleapis.com/unstoppable-client-assets-staging/campaigns/Unstoppable%20Marketplace/benjisplash.png",
+      "utm": "https://get.unstoppabledomains.com/benji/",
+      "title": "Who let the .Benji out?!!",
+      "subtitle": "We’ve teamed up with Basenji for our first-ever TLD partnership with a memecoin on Base network - Join the Basenji community today with .Benji!",
+      "button": "Get a .Benji!"
+    },
     {
       "image": "https://storage.googleapis.com/unstoppable-client-assets-staging/campaigns/Unstoppable%20Marketplace/Dream%20Launch%20Asset%20-%20Rectangular%20(1).png",
       "utm" : "https://domain.somnia.network/",
@@ -65,13 +72,6 @@ const Carousel: React.FC<PropType> = (props) => {
       "title": "Introducing .PW: The Future of Public Wallets",
       "subtitle": "We've partnered with Radix to launch .PW domains. Get a FREE .PW domain for 1 year with a purchase of a .crypto domain.",
       "button": "Get a .PW!"
-    },
-    {
-      "image": "https://storage.googleapis.com/unstoppable-client-assets-staging/campaigns/Unstoppable%20Marketplace/carousel-cryptocom-ai.png",
-      "utm": "https://crypto.com/nft/drops-event/a30ecbf68a9c0aa2ba97f8660c9488f7?tab=shop",
-      "title": "Live Now: AI-Themed Premium Drop with Crypto.com!",
-      "subtitle": "Our second drop is selling out fast! Priced at special crypto.com rates, domains are flying off the shelves. Get your hands on the remaining gems…",
-      "button": "Shop the drop!"
     }
   ]
 
