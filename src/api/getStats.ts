50c82adcc9c67a7019814d8fbccfc1d2d7ed3723
Vincent Shadbolt <vincent@unstoppabledomains.com>
2024-10-01 10:00:30 -0700

    Updated getStats.ts

--- src/api/getStats.ts
+++ src/api/getStats.ts
@@ -23,27 +23,29 @@ export const getStats = async () => {
       })
       const tickerCount = uniqueArray.length.toLocaleString()
 
-      var res = await fetch('https://api.unstoppabledomains.com/api/domains/taken-count')
-      var json = await res.json()
-      const takenDomainsCount = (json.takenDomainsCount/1000000).toLocaleString()+'M+'
+      // var res = await fetch('https://api.unstoppabledomains.com/api/domains/taken-count')
+      // var json = await res.json()
+      // const takenDomainsCount = (json.takenDomainsCount/1000000).toLocaleString()+'M+'
 
-      var res = await fetch('https://api.unstoppabledomains.com/api/domain/records/count')
-      var json = await res.json()
-      const recordsCount = Math.round(json.count/1000).toLocaleString()+(json.count/1000 % 1 >= 0.5 ? 'k+' : 'k')
+      // var res = await fetch('https://api.unstoppabledomains.com/api/domain/records/count')
+      // var json = await res.json()
+      // const recordsCount = Math.round(json.count/1000).toLocaleString()+(json.count/1000 % 1 >= 0.5 ? 'K+' : 'K')
 
       return {
         'tickers': tickerCount,
-        'domains': takenDomainsCount,
-        'records': recordsCount, 
+        // 'domains': takenDomainsCount,
+        // 'records': recordsCount, 
+        'domains': '4M+',
+        'records': '810K+', 
         'integrations': '865',
         'partners': '1000'
       }
     } catch (err) {
       //console.warn(err)
       return {
-        'tickers': '307',
-        'domains': '3.8M+',
-        'records': '778k', 
+        'tickers': '310',
+        'domains': '4M+',
+        'records': '810K+', 
         'integrations': '865',
         'partners': '1000'
       }
